6d68d8f39e8a86033f2d9b0e34e82a57eeecd2b5
Xileus <zachariah.minniecon@my.jcu.edu.au>
2015-03-16 00:14:43 +1000

    Sigh

--- src/App.java
+++ src/App.java
@@ -9,9 +9,33 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("BMI Manager");
         System.out.println("Please select from the following menu options:\n\t1. Add new patient\n\t2. View patient\n\t3. Exit");
-        aboutTime:while (true){
-        System.out.printf("Enter choice: ");
-        int choice = scanner.nextInt();
-        switch (choice){case 1:{System.out.println("Name:");String name = scanner.next();System.out.println("Age");int age = scanner.nextInt();System.out.println("Fatness, erhm, Weight");double weight = scanner.nextDouble();System.out.println("Height");double height = scanner.nextDouble();patient.setName(name);patient.setAge(age);patient.setDetails(height,weight);break;}case 2:System.out.println(String.format("Name: %s Age: %d BMI: %.2f", patient.getName(), patient.getAge(), patient.getBMI()));break;case 3:{System.out.println("Good riddance");break aboutTime;}}
-    }}
+        aboutTime:
+        while (true) {
+            System.out.printf("Enter choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1: {
+                    System.out.println("Name:");
+                    String name = scanner.next();
+                    System.out.println("Age");
+                    int age = scanner.nextInt();
+                    System.out.println("Fatness, erhm, Weight");
+                    double weight = scanner.nextDouble();
+                    System.out.println("Height");
+                    double height = scanner.nextDouble();
+                    patient.setName(name);
+                    patient.setAge(age);
+                    patient.setDetails(height, weight);
+                    break;
+                }
+                case 2:
+                    System.out.println(String.format("Name: %s Age: %d BMI: %.2f", patient.getName(), patient.getAge(), patient.getBMI()));
+                    break;
+                case 3: {
+                    System.out.println("Good riddance");
+                    break aboutTime;
+                }
+            }
+        }
+    }
 }
